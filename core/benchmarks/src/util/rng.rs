978f9d43980a0b3a701ca291bbc7f949e598fc23
Bernardo Pinto Gomes <bernardopgomes@hotmail.com>
2026-09-03 08:11:03 -0300

    fix(benchmarks): migrate rand generation APIs

--- core/benchmarks/src/util/rng.rs
+++ core/benchmarks/src/util/rng.rs
@@ -3,6 +3,6 @@ use rand::{rngs::StdRng, SeedableRng};
 pub fn rng_from_optional_seed(seed: Option<u64>) -> StdRng {
 	match seed {
 		Some(s) => StdRng::seed_from_u64(s),
-		None => StdRng::from_entropy(),
+		None => rand::make_rng(),
 	}
 }
